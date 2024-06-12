c33bc1859df5092435cf9b21b527cb237f064876
angelomaldora <143071138+angelomaldora@users.noreply.github.com>
2024-06-12 11:46:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const { ndown } = require('nayan-media-downloader');
+const { tikdown } = require('nayan-media-downloader');
+const { ytdown } = require('nayan-media-downloader');
+const { twitterdown } = require('nayan-media-downloader');
 
 const app = express();
 const port = 8266;
 
-app.get('/videodownloader', async (req, res) => {
+app.get('/fb', async (req, res) => {
     const { link } = req.query;
 
     if (!link) {
@@ -13,6 +16,53 @@ app.get('/videodownloader', async (req, res) => {
 
     try {
         let URL = await ndown(link);
+        res.send(URL);
+    } catch (error) {
+        console.error("Error downloading media:", error);
+        res.status(500).send(`Error downloading media: ${error.message}`);
+    }
+});
+app.get('/tiktok', async (req, res) => {
+    const { link } = req.query;
+
+    if (!link) {
+        return res.status(400).send('Link parameter is required.');
+    }
+
+    try {
+        let URL = await tikdown(link);
+        res.send(URL);
+    } catch (error) {
+        console.error("Error downloading media:", error);
+        res.status(500).send(`Error downloading media: ${error.message}`);
+    }
+});
+
+app.get('/yt', async (req, res) => {
+    const { link } = req.query;
+
+    if (!link) {
+        return res.status(400).send('Link parameter is required.');
+    }
+
+    try {
+        let URL = await ytdown(link);
+        res.send(URL);
+    } catch (error) {
+        console.error("Error downloading media:", error);
+        res.status(500).send(`Error downloading media: ${error.message}`);
+    }
+});
+
+app.get('/twitter', async (req, res) => {
+    const { link } = req.query;
+
+    if (!link) {
+        return res.status(400).send('Link parameter is required.');
+    }
+
+    try {
+        let URL = await twitterdown(link);
         res.send(URL);
     } catch (error) {
         console.error("Error downloading media:", error);
